662c8648fbe9892241639436306d84e305d96668
Nikola <n.maric1912@gmail.com>
2024-12-05 00:24:37 +0100

    feat: add drawSquare in OpenGLHelper

--- Drone/Drone/OpenGLHelper.h
+++ Drone/Drone/OpenGLHelper.h
@@ -9,5 +9,7 @@ unsigned int compileShader(GLenum type, const char* source);
 
 unsigned int loadTexture(const char* path);
 
+void drawSquare(unsigned int texture, unsigned int offset, unsigned int numOfPoints);
+
 
 
